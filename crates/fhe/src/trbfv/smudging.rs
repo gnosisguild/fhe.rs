97681cb2e934983a06883974cd9464450000c6d2
younestal1 <talibialaouiyounes@gmail.com>
2026-06-12 16:49:07 +0200

    fixing bound

--- crates/fhe/src/trbfv/smudging.rs
+++ crates/fhe/src/trbfv/smudging.rs
@@ -60,7 +60,7 @@ impl SmudgingBoundCalculatorConfig {
             m,
             b_enc,
             b_e: (2 * variance) as u64,
-            public_key_error: (2 * variance) as u64,
+            public_key_error: (n as u64) * (2 * variance) as u64,
             secret_key_bound: n as u64,
             lambda,
         }
@@ -246,7 +246,10 @@ mod tests {
         );
         // b_e is u64
         assert_eq!(config.b_e, (params.variance() * 2) as u64);
-        assert_eq!(config.public_key_error, 2 * params.variance() as u64);
+        assert_eq!(
+            config.public_key_error,
+            (config.n as u64) * (2 * params.variance()) as u64
+        );
         assert_eq!(config.secret_key_bound, 5);
         assert_eq!(config.lambda, 80);
     }
